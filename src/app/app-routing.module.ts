a45b1e6613c97c846711482724db4bf582386977
geesalasandeep <geesalasandeep@gmail.com>
2022-01-25 19:00:43 +0530

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataComponent } from './data/data.component';
+import { DemoComponent } from './demo/demo.component';
+import { FirstComponent } from './first/first.component';
+import { InteractComponent } from './interact/interact.component';
+import { NewreactiveComponent } from './newreactive/newreactive.component';
+import { PipeComponent } from './pipe/pipe.component';
+import { ReactiveComponent } from './reactive/reactive.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { SubjectComponent } from './subject/subject.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { ToArrayComponent } from './to-array/to-array.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  {path:"template",component: TemplateDrivenComponent},
+  {path:"reactive",component: ReactiveComponent},
+  {path:"newreactive",component: NewreactiveComponent},  
+  {path:"pipes",component: PipeComponent},
+  {path:"data",component:DataComponent},
+  {path:"interact",component:InteractComponent },
+  {path:"rxjs",component:RxjsComponent},
+  {path:"toarray",component:ToArrayComponent},
+  {path:'subject',component:SubjectComponent},
+  {path : 'first',component:FirstComponent },
+  {path:'demo',component:DemoComponent},
+  {path:'interact',component:InteractComponent}
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { ReactiveComponent } from './reactive/reactive.component';
+import { NewreactiveComponent } from './newreactive/newreactive.component';
+import { PipesPipe } from './pipes.pipe';
+import { PipeComponent } from './pipe/pipe.component';
+import { EmployeePipe } from './employee.pipe';
+import { DataComponent } from './data/data.component';
+import { InteractComponent } from './interact/interact.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { IntervalComponent } from './interval/interval.component';
+import { ToArrayComponent } from './to-array/to-array.component';
+import { FirstComponent } from './first/first.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenComponent,
+    ReactiveComponent,
+    NewreactiveComponent,
+    PipesPipe,
+    PipeComponent,
+    EmployeePipe,
+    DataComponent,
+    InteractComponent,
+    RxjsComponent,
+    IntervalComponent,
+    ToArrayComponent,
+    FirstComponent,
+    DemoComponent,
+  
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
